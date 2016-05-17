c1cd56509ad0c9c7b538226fac2f5c03a4ba9afe
ninglvfeihong <ninglvfeihong@gmail.com>
2016-05-17 18:48:18 +0100

    Add AT+CSLOCK framework: child safety lock 	AT+CSLOCK:XXXX,0 disable the child safety lock 	AT+CSLOCK:XXXX,1 enable the child safety lock 	AT+CSLOCK:XXXX,2 toggle the child safety lock 	AT+CSLOCK:XXXX,x just return the child safety lock status  Changes to be committed: 	modified:   Projects/zstack/AT commands/project/Source/AT_AF.c 	modified:   Projects/zstack/AT commands/project/Source/AT_AF.h 	modified:   Projects/zstack/AT commands/project/Source/AT_uart.c 	modified:   Projects/zstack/AT commands/project/Source/AT_uart.h

--- Projects/zstack/AT commands/project/Source/AT_AF.h
+++ Projects/zstack/AT commands/project/Source/AT_AF.h
@@ -12,7 +12,7 @@
 #define AT_AF_enable             1
 #define AT_AF_disable            0
 
-#define AT_AF_MAX_CLUSTERS                 8
+#define AT_AF_MAX_CLUSTERS                 10
 #define AT_AF_Cmd_REPPRINT_CLUSTERID       0
 #define AT_AF_Cmd_REPENABLE_CLUSTERID      1
 #define AT_AF_Cmd_HA_DISC_CLUSTERID        2
@@ -22,6 +22,7 @@
 #define AT_AF_POWER_SVING_EXP_CLUSTERID    6
 #define AT_AF_UPDATE_CLUSTERID             7
 #define AT_AF_DEV_REPORT_CLUSTERID         8
+#define AT_AF_CSLOCK_CLUSTERID             9
 #define AT_AF_GROUP_ID                     0x8000
 
 
@@ -36,7 +37,8 @@
   AT_AF_Cmd_R_CIDDISC_CLUSTERID,                \
   AT_AF_POWER_SVING_EXP_CLUSTERID,              \
   AT_AF_UPDATE_CLUSTERID,                       \
-  AT_AF_DEV_REPORT_CLUSTERID                    \
+  AT_AF_DEV_REPORT_CLUSTERID,                   \
+  AT_AF_CSLOCK_CLUSTERID                        \
 }
 
 
